08ad3e66a43b7ec873113abd64bb1869547c9fb7
moyotsukai <gyoshu.bowie@gmail.com>
2023-06-09 23:33:23 +0900

    Initial Commit

---
+++ MusicAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicAPI
+//
+//  Created by Owner on 2023/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
